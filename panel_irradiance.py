4fd7544e44e6ae5df04b8bd7e97e5599835eab91
qazwsxal <qazwsxalan@gmail.com>
2017-01-18 10:21:32 +0000

    Added preliminary panel data and finalised irradiance calculations

--- panel_irradiance.py
+++ panel_irradiance.py
@@ -6,6 +6,7 @@ import ephem
 import gpxpy
 import gpxpy.gpx
 import matplotlib.pyplot as plt
+import numpy as np
 # import pytz
 
 
@@ -63,6 +64,37 @@ gpx_file = open('wsc.gpx', 'r')
 START = dt.datetime(2017, 10, 10, 22, 30)
 END = dt.datetime(2017, 10, 11, 7, 30)
 
+pandists = [-4215.41,
+            -3726.01,
+            -3656.01,
+            -3217.64,
+            -3044.60,
+            -2696.64,
+            -2378.26,
+            -2193.80,
+            -1746.41,
+            -1669.14,
+            -1157.79,
+            -1082.44,
+            -650.51,
+            -481.27]
+panvects = np.array([[-25.13, 0, 227.45],
+                    [-31.48, 0, 254.43],
+                    [-27.76, 0, 227.09],
+                    [-26.44, 0, 263.87],
+                    [-27.47, 0, 318.31],
+                    [-17.84, 0, 438.92],
+                    [-5.57, 0, 331.90],
+                    [-1.97, 0, 329.76],
+                    [4.85, 0, 267.36],
+                    [7.44, 0, 334.09],
+                    [12.28, 0, 236.57],
+                    [13.72, 0, 241.07],
+                    [21.56, 0, 228.41]])
+
+# print(np.sum(panvects, axis=1))
+panvects = panvects/np.linalg.norm(panvects, axis=1)[:, None]
+
 gpx = gpxpy.parse(gpx_file)
 path = gpx.tracks[0].segments[0].points
 # for track in gpx.tracks:
@@ -91,18 +123,28 @@ for i in range(len(path)-1):
 alts = []
 azs = []
 car_position = ephem.Observer()
+print("Calculating Sun Positions")
 for i in range(len(dists)):
     car_position.lon = str(path[i].longitude)
     car_position.lat = str(path[i].latitude)
     car_position.elevation = path[i].elevation
     if i % 100 == 0:
         print(100*i/len(dists), '%')
-    for j in range(100):
+    for j in range(20):
         car_position.date = random_time(START, END)
         sun = ephem.Sun(car_position)
         alts.append(sun.alt)
         # Work out sun position relative to car
         azs.append((sun.az - math.radians(bearings[i])) % 2*math.pi)
 
-sunvects = list(map(altaz2vec, alts, azs))
-print(sunvects)
+print("Determing Sun Vectors")
+sunvects = np.transpose(np.array(list(map(altaz2vec, alts, azs))))
+print("Calculating Irradiances")
+irrad = np.dot(panvects, sunvects)
+result = np.sum(irrad, axis=1)/sunvects.shape[1]
+print(result.shape)
+print(len(pandists))
+# plt.plot(pandists, result)
+plt.plot(result/np.linalg.norm(result, ord=np.inf))
+# plt.plot(pandists/np.linalg.norm(pandists, ord=np.inf))
+plt.show()
