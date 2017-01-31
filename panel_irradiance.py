ce5623cf6b5c141809b15c98fd28a5cd53faf12a
qazwsxal <qazwsxalan@gmail.com>
2017-01-31 15:44:15 +0000

    use car curve data for normals

--- panel_irradiance.py
+++ panel_irradiance.py
@@ -1,6 +1,7 @@
 import math
 import random
 import datetime as dt
+import csv
 
 import ephem
 import gpxpy
@@ -57,6 +58,17 @@ def altaz2vec(altitude, azimuth):
     return [x, y, z]
 
 
+
+panel_curve = []
+with open('SolarCarBody_Pressure.txt', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        panel_curve.append(list(map(lambda x: float(x.strip('m')), row))[0:2])
+panel_curve = np.array(panel_curve)
+panel_grads = np.diff(panel_curve, axis=0)
+panel_norms = np.transpose(np.dot(np.array([[0, 1],[0, 0], [-1, 0]]), np.transpose(panel_grads)))
+panel_norms = panel_norms/np.linalg.norm(panel_norms, axis=1)[:, None]
+print(panel_norms)
 gpx_file = open('wsc.gpx', 'r')
 
 
@@ -64,36 +76,7 @@ gpx_file = open('wsc.gpx', 'r')
 START = dt.datetime(2017, 10, 10, 22, 30)
 END = dt.datetime(2017, 10, 11, 7, 30)
 
-pandists = [-4215.41,
-            -3726.01,
-            -3656.01,
-            -3217.64,
-            -3044.60,
-            -2696.64,
-            -2378.26,
-            -2193.80,
-            -1746.41,
-            -1669.14,
-            -1157.79,
-            -1082.44,
-            -650.51,
-            -481.27]
-panvects = np.array([[-25.13, 0, 227.45],
-                    [-31.48, 0, 254.43],
-                    [-27.76, 0, 227.09],
-                    [-26.44, 0, 263.87],
-                    [-27.47, 0, 318.31],
-                    [-17.84, 0, 438.92],
-                    [-5.57, 0, 331.90],
-                    [-1.97, 0, 329.76],
-                    [4.85, 0, 267.36],
-                    [7.44, 0, 334.09],
-                    [12.28, 0, 236.57],
-                    [13.72, 0, 241.07],
-                    [21.56, 0, 228.41]])
-
 # print(np.sum(panvects, axis=1))
-panvects = panvects/np.linalg.norm(panvects, axis=1)[:, None]
 
 gpx = gpxpy.parse(gpx_file)
 path = gpx.tracks[0].segments[0].points
@@ -130,7 +113,7 @@ for i in range(len(dists)):
     car_position.elevation = path[i].elevation
     if i % 100 == 0:
         print(100*i/len(dists), '%')
-    for j in range(20):
+    for j in range(100):
         car_position.date = random_time(START, END)
         sun = ephem.Sun(car_position)
         alts.append(sun.alt)
@@ -140,11 +123,22 @@ for i in range(len(dists)):
 print("Determing Sun Vectors")
 sunvects = np.transpose(np.array(list(map(altaz2vec, alts, azs))))
 print("Calculating Irradiances")
-irrad = np.dot(panvects, sunvects)
+irrad = np.dot(panel_norms, sunvects)
 result = np.sum(irrad, axis=1)/sunvects.shape[1]
-print(result.shape)
-print(len(pandists))
+
+plane = np.array([[0,0,1],[0,0,1]])
+plane_dims = np.array([panel_curve[-2,0], panel_curve[0,0]])
+plane_irrad = np.dot(plane,sunvects)
+plane_result = np.sum(plane_irrad, axis=1)/sunvects.shape[1]
 # plt.plot(pandists, result)
-plt.plot(result/np.linalg.norm(result, ord=np.inf))
+plt.subplot(211)
+# plt.plot(panel_curve[:-1,0],result/np.linalg.norm(result, ord=np.inf))
+plt.plot(panel_curve[:-1,0],result)
+plt.plot(plane_dims,plane_result)
+plt.ylim(0.7,0.8)
+plt.subplot(212, aspect='equal')
+plt.plot(panel_curve[:-1,0],panel_curve[:-1,1])
+plt.ylim(-20,220)
+# plt.axis('equal')
 # plt.plot(pandists/np.linalg.norm(pandists, ord=np.inf))
 plt.show()
